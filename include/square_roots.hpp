7a95c7e9fdf7184eb0c214c759e70a0540369130
a8a3 <you@example.com>
2025-10-11 22:01:33 +0200

    simple test + CI

---
+++ include/square_roots.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+// minimal header-only sum function used by tests
+inline int sum(int a, int b) {
+    return a + b;
+}
---
+++ test/square_roots_test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include <square_roots.hpp>
+
+TEST(SquareRoots, Basic) {
+    EXPECT_EQ(sum(2, 3), 5);
+}
